2074061078d01e4a5b854e1c9685651d8136b8ac
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-08-09 16:39:05 +0100

    Add JavaScript Code To Remove Book From UI

--- books.js
+++ books.js
@@ -1,3 +1,4 @@
+// Books Array
 const awesomeBooks = [
   {
     title: 'Things Fall Apart',
@@ -12,11 +13,12 @@ const awesomeBooks = [
 
 const bookSection = document.getElementById('awesome-books');
 
+// Div that contains book HTML
 function showBooks(book) {
-  const bookList = `<div>
+  const bookList = `<div id="book-info">
   <p>${book.title}</p>
   <p>${book.author}</p>
-  <button type="submit" id="remove-book">Remove</button>
+  <button type="submit" id="remove-book" onclick="removeItem()">Remove</button>
   <hr>
   </div>
   `;
@@ -24,11 +26,12 @@ function showBooks(book) {
   return bookList;
 }
 
+// Add New Book To List
+
 const titleInput = document.getElementById('book-title');
 const authorInput = document.getElementById('book-author');
 const form = document.getElementById('form');
 
-// Add New Book To List
 function addBookToList(e) {
   e.preventDefault();
   const newBook = {
@@ -37,14 +40,22 @@ function addBookToList(e) {
   };
   bookSection.innerHTML += showBooks(newBook);
 }
-form.addEventListener('submit', addBookToList)
+form.addEventListener('submit', addBookToList);
 
+// Display Books in UI
 function showMyBooks() {
   const myBooks = Object.keys(awesomeBooks);
   const myBooksLength = Object.keys(awesomeBooks).length;
   for (let i = 0; i < myBooksLength; i += 1) {
     bookSection.innerHTML += showBooks(awesomeBooks[myBooks[i]], i);
   }
+}
+
+// Remove Books
+/* eslint-disable no-unused-vars */
+function removeItem() {
+  const removeBook = document.getElementById('book-info');
+  removeBook.parentNode.removeChild(removeBook);
 }
 
 window.onload = showMyBooks;
